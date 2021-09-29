62aed725c276bc5423a82cf823374b56eb0a978d
王劲鹏 <jingpeng.wang@outlook.com>
2021-09-29 19:34:54 +0800

    update code

--- example/src/webgl_effects_peppersghost.ts
+++ example/src/webgl_effects_peppersghost.ts
@@ -37,98 +37,110 @@ class webgl_effects_peppersghost extends Panel {
                 clientHeight: height,
                 getContext: (() => {return gl}) as any,
               } as HTMLCanvasElement);
-
+              let window = {
+                innerWidth: width,
+                innerHeight: height,
+                devicePixelRatio: 1,
+                addEventListener: (() => {}) as any
+              }
 
               //#region code to impl
 
+              let container;
+
               let camera, scene, renderer, effect;
               let group;
-        
+
               init();
               animate();
-        
+
               function init() {
-        
-                camera = new THREE.PerspectiveCamera( 60, width / height, 1, 100000 );
-        
+
+                // container = document.createElement( 'div' );
+                // document.body.appendChild( container );
+
+                camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
+
                 scene = new THREE.Scene();
-        
+
                 group = new THREE.Group();
                 scene.add( group );
-        
+
                 // Cube
-        
+
                 const geometry = new THREE.BoxGeometry().toNonIndexed(); // ensure unique vertices for each triangle
-        
+
                 const position = geometry.attributes.position;
                 const colors: any[] = [];
                 const color = new THREE.Color();
-        
+
                 // generate for each side of the cube a different color
-        
+
                 for ( let i = 0; i < position.count; i += 6 ) {
-        
+
                   color.setHex( Math.random() * 0xffffff );
-        
+
                   // first face
-        
+
                   colors.push( color.r, color.g, color.b );
                   colors.push( color.r, color.g, color.b );
                   colors.push( color.r, color.g, color.b );
-        
+
                   // second face
-        
+
                   colors.push( color.r, color.g, color.b );
                   colors.push( color.r, color.g, color.b );
                   colors.push( color.r, color.g, color.b );
-        
+
                 }
-        
+
                 geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
-        
+
                 const material = new THREE.MeshBasicMaterial( { vertexColors: true } );
-        
+
                 for ( let i = 0; i < 10; i ++ ) {
-        
+
                   const cube = new THREE.Mesh( geometry, material );
                   cube.position.x = Math.random() * 2 - 1;
                   cube.position.y = Math.random() * 2 - 1;
                   cube.position.z = Math.random() * 2 - 1;
                   cube.scale.multiplyScalar( Math.random() + 0.5 );
                   group.add( cube );
-        
+
                 }
-        
+
                 renderer = new THREE.WebGLRenderer({canvas: inputCanvas});
-                renderer.setPixelRatio( 1 );
-        
+                renderer.setPixelRatio( window.devicePixelRatio );
+                // container.appendChild( renderer.domElement );
+
                 effect = new PeppersGhostEffect( renderer );
-                effect.setSize( width, height );
+                effect.setSize( window.innerWidth, window.innerHeight );
                 effect.cameraDistance = 5;
-        
-                // window.addEventListener( 'resize', onWindowResize );
-        
+
+                window.addEventListener( 'resize', onWindowResize );
+
               }
-        
+
               function onWindowResize() {
-        
+
                 camera.aspect = window.innerWidth / window.innerHeight;
                 camera.updateProjectionMatrix();
-        
+
                 effect.setSize( window.innerWidth, window.innerHeight );
-        
+
               }
 
               function animate() {
-        
+
                 requestAnimationFrame( animate );
-        
+
                 group.rotation.y += 0.01;
-        
+
                 effect.render( scene, camera );
-  
+
                 gl.flush();
                 gl.endFrameEXP();
+
               }
 
               //#endregion
