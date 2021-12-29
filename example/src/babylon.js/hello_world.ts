caad97bcae4abb776c2273ebda795172e9c2102b
王劲鹏 <jingpeng.wang@outlook.com>
2021-12-29 16:15:51 +0800

    fix

--- example/src/babylon.js/hello_world.ts
+++ example/src/babylon.js/hello_world.ts
@@ -5,94 +5,163 @@ import {
   layoutConfig,
   Gravity,
   navbar,
-  stack,
+  loge,
+  gestureContainer,
+  GestureContainer,
 } from "doric";
 import { dangleView, DangleWebGLRenderingContext, vsync } from "dangle";
 
-const global = new Function('return this')()
+const global = new Function("return this")();
 global.window = {
   devicePixelRatio: 1,
-  addEventListener: (() => {}) as any,
+  addEventListener: ((name) => {
+    loge("window.addEventListener: " + name);
+  }) as any,
   navigator: {
-    appVersion: '5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
-    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+    appVersion:
+      "5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+    userAgent:
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
   },
   requestAnimationFrame: vsync(context).requestAnimationFrame,
-  cancelAnimationFrame: vsync(context).cancelAnimationFrame
-}
-global.navigator = global.window.navigator
+  cancelAnimationFrame: vsync(context).cancelAnimationFrame,
+};
+global.navigator = global.window.navigator;
 global.document = {
-  addEventListener: (() => {}) as any,
-}
+  addEventListener: ((name) => {
+    loge("document.addEventListener: " + name);
+  }) as any,
+  createElement: (() => {
+    return {};
+  }) as any,
+};
 
-import * as BABYLON from 'babylonjs'
+import * as BABYLON from "babylonjs";
 
 @Entry
 class hello_world extends Panel {
+  private gestureView?: GestureContainer;
+
   onShow() {
     navbar(context).setTitle("hello_world");
   }
   build(rootView: Group) {
+    let self = this;
     vlayout([
-      stack(
+      (this.gestureView = gestureContainer(
         [
           dangleView({
             onReady: (gl: DangleWebGLRenderingContext) => {
-              const width = gl.drawingBufferWidth
-              const height = gl.drawingBufferHeight
+              const width = gl.drawingBufferWidth;
+              const height = gl.drawingBufferHeight;
 
-              const canvas = 
-              ({
+              const canvas = {
                 width: width,
                 height: height,
                 style: {},
-                addEventListener: (() => {}) as any,
+                addEventListener: ((name, listener) => {
+                  loge("canvas.addEventListener: " + name);
+                  loge(listener);
+                  if (name == "mousedown") {
+                    self.gestureView!!.onTouchDown = ({ x, y }) => {
+                      listener({
+                        clientX: x * Environment.screenScale,
+                        clientY: y * Environment.screenScale,
+                        pointerType: "touch",
+                        preventDefault: (() => {
+                          return {};
+                        }) as any,
+                      });
+                    };
+                  } else if (name == "mousemove") {
+                    self.gestureView!!.onTouchMove = ({ x, y }) => {
+                      listener({
+                        clientX: x * Environment.screenScale,
+                        clientY: y * Environment.screenScale,
+                        pointerType: "touch",
+                        preventDefault: (() => {
+                          return {};
+                        }) as any,
+                      });
+                    };
+                  }
+                }) as any,
                 removeEventListener: (() => {}) as any,
+                focus: (() => {}) as any,
                 clientHeight: height,
-                getContext: (() => {return gl}) as any,
-                getBoundingClientRect: (() => {return {
-                  width: width,
-                  height: height,
-                }}) as any
-              } as HTMLCanvasElement);
+                getContext: (() => {
+                  return gl;
+                }) as any,
+                getBoundingClientRect: (() => {
+                  return {
+                    left: 0,
+                    top: 0,
+                    width: width,
+                    height: height,
+                  };
+                }) as any,
+              } as HTMLCanvasElement;
 
-              global.window.innerWidth = width
-              global.window.innerHeight = height
+              global.window.innerWidth = width;
+              global.window.innerHeight = height;
 
               //#region code to impl
               // Load the 3D engine
-              var engine = new BABYLON.Engine(gl as any, true, {preserveDrawingBuffer: true, stencil: true});
+              var engine = new BABYLON.Engine(canvas, true, {
+                preserveDrawingBuffer: true,
+                stencil: true,
+              });
               // CreateScene function that creates and return the scene
-              var createScene = function(){
+              var createScene = function () {
                 // Create a basic BJS Scene object
                 var scene = new BABYLON.Scene(engine);
                 // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
-                var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+                var camera = new BABYLON.FreeCamera(
+                  "camera1",
+                  new BABYLON.Vector3(0, 5, -10),
+                  scene
+                );
                 // Target the camera to scene origin
                 camera.setTarget(BABYLON.Vector3.Zero());
                 // Attach the camera to the canvas
                 camera.attachControl(canvas, false);
                 // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
-                var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+                var light = new BABYLON.HemisphericLight(
+                  "light1",
+                  new BABYLON.Vector3(0, 1, 0),
+                  scene
+                );
                 // Create a built-in "sphere" shape using the SphereBuilder
-                var sphere = BABYLON.MeshBuilder.CreateSphere('sphere1', {segments: 16, diameter: 2, sideOrientation: BABYLON.Mesh.FRONTSIDE}, scene);
+                var sphere = BABYLON.MeshBuilder.CreateSphere(
+                  "sphere1",
+                  {
+                    segments: 16,
+                    diameter: 2,
+                    sideOrientation: BABYLON.Mesh.FRONTSIDE,
+                  },
+                  scene
+                );
                 // Move the sphere upward 1/2 of its height
                 sphere.position.y = 1;
                 // Create a built-in "ground" shape;
-                var ground = BABYLON.MeshBuilder.CreateGround("ground1", { width: 6, height: 6, subdivisions: 2, updatable: false }, scene);
+                var ground = BABYLON.MeshBuilder.CreateGround(
+                  "ground1",
+                  { width: 6, height: 6, subdivisions: 2, updatable: false },
+                  scene
+                );
                 // Return the created scene
                 return scene;
-              }
+              };
               // call the createScene function
               var scene = createScene();
               // run the render loop
-              engine.runRenderLoop(function(){
+              engine.runRenderLoop(function () {
                 scene.render();
 
                 gl.endFrame();
               });
               // the canvas/window resize event handler
-              window.addEventListener('resize', function(){
+              window.addEventListener("resize", function () {
                 engine.resize();
               });
 
@@ -109,7 +178,7 @@ class hello_world extends Panel {
           width: 300,
           height: 300,
         }
-      ),
+      )),
     ])
       .apply({
         layoutConfig: layoutConfig().fit().configAlignment(Gravity.Center),
