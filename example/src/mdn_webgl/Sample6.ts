fc43bee5734c33a9a258632b84426c3954e0c3c0
王劲鹏 <jingpeng.wang@outlook.com>
2022-01-07 10:32:25 +0800

    update sample6 & format

--- example/src/mdn_webgl/Sample6.ts
+++ example/src/mdn_webgl/Sample6.ts
@@ -13,8 +13,6 @@ import { dangleView, DangleWebGLRenderingContext, vsync } from "dangle";
 import { mat4 } from "gl-matrix";
 
 var cubeRotation = 0.0;
-;
-
 @Entry
 class Sample6 extends Panel {
   onShow() {
@@ -29,10 +27,12 @@ class Sample6 extends Panel {
               (gl as any).canvas = {
                 clientWidth: gl.drawingBufferWidth,
                 clientHeight: gl.drawingBufferHeight,
-              }
+              };
+              const requestAnimationFrame =
+                vsync(context).requestAnimationFrame;
 
               //#region code to impl
-              
+
               // Vertex shader program
 
               const vsSource = `
@@ -68,27 +68,39 @@ class Sample6 extends Panel {
               const programInfo = {
                 program: shaderProgram,
                 attribLocations: {
-                  vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
-                  textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
+                  vertexPosition: gl.getAttribLocation(
+                    shaderProgram,
+                    "aVertexPosition"
+                  ),
+                  textureCoord: gl.getAttribLocation(
+                    shaderProgram,
+                    "aTextureCoord"
+                  ),
                 },
                 uniformLocations: {
-                  projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
-                  modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
-                  uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
-                }
+                  projectionMatrix: gl.getUniformLocation(
+                    shaderProgram,
+                    "uProjectionMatrix"
+                  ),
+                  modelViewMatrix: gl.getUniformLocation(
+                    shaderProgram,
+                    "uModelViewMatrix"
+                  ),
+                  uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
+                },
               };
 
               // Here's where we call the routine that builds all the
               // objects we'll be drawing.
               const buffers = initBuffers(gl);
 
-              const texture = await loadTexture(gl, 'cubetexture.png');
+              const texture = await loadTexture(gl, "cubetexture.png");
 
               var then = 0;
 
               // Draw the scene repeatedly
               function render(now) {
-                now *= 0.001;  // convert to seconds
+                now *= 0.001; // convert to seconds
                 const deltaTime = now - then;
                 then = now;
 
@@ -96,9 +108,9 @@ class Sample6 extends Panel {
 
                 gl.endFrame();
 
-                vsync(context).requestAnimationFrame(render);
+                requestAnimationFrame(render);
               }
-              vsync(context).requestAnimationFrame(render);
+              requestAnimationFrame(render);
 
               //#endregion
             },
@@ -123,7 +135,7 @@ class Sample6 extends Panel {
       .in(rootView);
   }
 }
-  
+
 //
 // initBuffers
 //
@@ -131,7 +143,6 @@ class Sample6 extends Panel {
 // have one object -- a simple three-dimensional cube.
 //
 function initBuffers(gl) {
-
   // Create a buffer for the cube's vertex positions.
 
   const positionBuffer = gl.createBuffer();
@@ -145,40 +156,22 @@ function initBuffers(gl) {
 
   const positions = [
     // Front face
-    -1.0, -1.0,  1.0,
-     1.0, -1.0,  1.0,
-     1.0,  1.0,  1.0,
-    -1.0,  1.0,  1.0,
+    -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
 
     // Back face
-    -1.0, -1.0, -1.0,
-    -1.0,  1.0, -1.0,
-     1.0,  1.0, -1.0,
-     1.0, -1.0, -1.0,
+    -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
 
     // Top face
-    -1.0,  1.0, -1.0,
-    -1.0,  1.0,  1.0,
-     1.0,  1.0,  1.0,
-     1.0,  1.0, -1.0,
+    -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
 
     // Bottom face
-    -1.0, -1.0, -1.0,
-     1.0, -1.0, -1.0,
-     1.0, -1.0,  1.0,
-    -1.0, -1.0,  1.0,
+    -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
 
     // Right face
-     1.0, -1.0, -1.0,
-     1.0,  1.0, -1.0,
-     1.0,  1.0,  1.0,
-     1.0, -1.0,  1.0,
+    1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
 
     // Left face
-    -1.0, -1.0, -1.0,
-    -1.0, -1.0,  1.0,
-    -1.0,  1.0,  1.0,
-    -1.0,  1.0, -1.0,
+    -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
   ];
 
   // Now pass the list of positions into WebGL to build the
@@ -194,39 +187,24 @@ function initBuffers(gl) {
 
   const textureCoordinates = [
     // Front
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
     // Back
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
     // Top
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
     // Bottom
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
     // Right
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
     // Left
-    0.0,  0.0,
-    1.0,  0.0,
-    1.0,  1.0,
-    0.0,  1.0,
+    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
   ];
 
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
-                gl.STATIC_DRAW);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(textureCoordinates),
+    gl.STATIC_DRAW
+  );
 
   // Build the element array buffer; this specifies the indices
   // into the vertex arrays for each face's vertices.
@@ -239,18 +217,51 @@ function initBuffers(gl) {
   // position.
 
   const indices = [
-    0,  1,  2,      0,  2,  3,    // front
-    4,  5,  6,      4,  6,  7,    // back
-    8,  9,  10,     8,  10, 11,   // top
-    12, 13, 14,     12, 14, 15,   // bottom
-    16, 17, 18,     16, 18, 19,   // right
-    20, 21, 22,     20, 22, 23,   // left
+    0,
+    1,
+    2,
+    0,
+    2,
+    3, // front
+    4,
+    5,
+    6,
+    4,
+    6,
+    7, // back
+    8,
+    9,
+    10,
+    8,
+    10,
+    11, // top
+    12,
+    13,
+    14,
+    12,
+    14,
+    15, // bottom
+    16,
+    17,
+    18,
+    16,
+    18,
+    19, // right
+    20,
+    21,
+    22,
+    20,
+    22,
+    23, // left
   ];
 
   // Now send the element array to GL
 
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-      new Uint16Array(indices), gl.STATIC_DRAW);
+  gl.bufferData(
+    gl.ELEMENT_ARRAY_BUFFER,
+    new Uint16Array(indices),
+    gl.STATIC_DRAW
+  );
 
   return {
     position: positionBuffer,
@@ -279,41 +290,61 @@ async function loadTexture(gl, url) {
   const border = 0;
   const srcFormat = gl.RGBA;
   const srcType = gl.UNSIGNED_BYTE;
-  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
-  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-                width, height, border, srcFormat, srcType,
-                pixel);
+  const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    level,
+    internalFormat,
+    width,
+    height,
+    border,
+    srcFormat,
+    srcType,
+    pixel
+  );
 
-  const remoteResource = new RemoteResource("https://doric.pub/about/The_Parthenon_in_Athens.jpg")
-  const info = await imageDecoder(context).getImageInfo(remoteResource)
+  const remoteResource = new RemoteResource(
+    "https://doric.pub/about/The_Parthenon_in_Athens.jpg"
+  );
+  const info = await imageDecoder(context).getImageInfo(remoteResource);
 
   const w = info.width;
   const h = info.height;
-  const arrayBuffer = await imageDecoder(context).decodeToPixels(remoteResource);
+  const arrayBuffer = await imageDecoder(context).decodeToPixels(
+    remoteResource
+  );
 
   // const image = new Image();
   // image.onload = function() {
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-      w, h, border, srcFormat, srcType,
-      arrayBuffer);
-    
-    // gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-    //               srcFormat, srcType, image);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    level,
+    internalFormat,
+    w,
+    h,
+    border,
+    srcFormat,
+    srcType,
+    arrayBuffer
+  );
 
-    // WebGL1 has different requirements for power of 2 images
-    // vs non power of 2 images so check if the image is a
-    // power of 2 in both dimensions.
-    if (isPowerOf2(w) && isPowerOf2(h)) {
-       // Yes, it's a power of 2. Generate mips.
-       gl.generateMipmap(gl.TEXTURE_2D);
-    } else {
-       // No, it's not a power of 2. Turn of mips and set
-       // wrapping to clamp to edge
-       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    }
+  // gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+  //               srcFormat, srcType, image);
+
+  // WebGL1 has different requirements for power of 2 images
+  // vs non power of 2 images so check if the image is a
+  // power of 2 in both dimensions.
+  if (isPowerOf2(w) && isPowerOf2(h)) {
+    // Yes, it's a power of 2. Generate mips.
+    gl.generateMipmap(gl.TEXTURE_2D);
+  } else {
+    // No, it's not a power of 2. Turn of mips and set
+    // wrapping to clamp to edge
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  }
   // };
   // image.src = url;
 
@@ -328,10 +359,10 @@ function isPowerOf2(value) {
 // Draw the scene.
 //
 function drawScene(gl, programInfo, buffers, texture, deltaTime) {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-  gl.clearDepth(1.0);                 // Clear everything
-  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+  gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
+  gl.clearDepth(1.0); // Clear everything
+  gl.enable(gl.DEPTH_TEST); // Enable depth testing
+  gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 
   // Clear the canvas before we start drawing on it.
 
@@ -344,7 +375,7 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
   // and we only want to see objects between 0.1 units
   // and 100 units away from the camera.
 
-  const fieldOfView = 45 * Math.PI / 180;   // in radians
+  const fieldOfView = (45 * Math.PI) / 180; // in radians
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   const zNear = 0.1;
   const zFar = 100.0;
@@ -352,11 +383,7 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
 
   // note: glmatrix.js always has the first argument
   // as the destination to receive the result.
-  mat4.perspective(projectionMatrix,
-                   fieldOfView,
-                   aspect,
-                   zNear,
-                   zFar);
+  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
 
   // Set the drawing position to the "identity" point, which is
   // the center of the scene.
@@ -365,17 +392,23 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
   // Now move the drawing position a bit to where we want to
   // start drawing the square.
 
-  mat4.translate(modelViewMatrix,     // destination matrix
-                 modelViewMatrix,     // matrix to translate
-                 [-0.0, 0.0, -6.0]);  // amount to translate
-  mat4.rotate(modelViewMatrix,  // destination matrix
-              modelViewMatrix,  // matrix to rotate
-              cubeRotation,     // amount to rotate in radians
-              [0, 0, 1]);       // axis to rotate around (Z)
-  mat4.rotate(modelViewMatrix,  // destination matrix
-              modelViewMatrix,  // matrix to rotate
-              cubeRotation * .7,// amount to rotate in radians
-              [0, 1, 0]);       // axis to rotate around (X)
+  mat4.translate(
+    modelViewMatrix, // destination matrix
+    modelViewMatrix, // matrix to translate
+    [-0.0, 0.0, -6.0]
+  ); // amount to translate
+  mat4.rotate(
+    modelViewMatrix, // destination matrix
+    modelViewMatrix, // matrix to rotate
+    cubeRotation, // amount to rotate in radians
+    [0, 0, 1]
+  ); // axis to rotate around (Z)
+  mat4.rotate(
+    modelViewMatrix, // destination matrix
+    modelViewMatrix, // matrix to rotate
+    cubeRotation * 0.7, // amount to rotate in radians
+    [0, 1, 0]
+  ); // axis to rotate around (X)
 
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute
@@ -387,14 +420,14 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
     const offset = 0;
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexPosition,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    gl.enableVertexAttribArray(
-        programInfo.attribLocations.vertexPosition);
+      programInfo.attribLocations.vertexPosition,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
   }
 
   // Tell WebGL how to pull out the texture coordinates from
@@ -407,14 +440,14 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
     const offset = 0;
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
     gl.vertexAttribPointer(
-        programInfo.attribLocations.textureCoord,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    gl.enableVertexAttribArray(
-        programInfo.attribLocations.textureCoord);
+      programInfo.attribLocations.textureCoord,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+    gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
   }
 
   // Tell WebGL which indices to use to index the vertices
@@ -427,13 +460,15 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
   // Set the shader uniforms
 
   gl.uniformMatrix4fv(
-      programInfo.uniformLocations.projectionMatrix,
-      false,
-      projectionMatrix);
+    programInfo.uniformLocations.projectionMatrix,
+    false,
+    projectionMatrix
+  );
   gl.uniformMatrix4fv(
-      programInfo.uniformLocations.modelViewMatrix,
-      false,
-      modelViewMatrix);
+    programInfo.uniformLocations.modelViewMatrix,
+    false,
+    modelViewMatrix
+  );
 
   // Specify the texture to map onto the faces.
 
@@ -475,7 +510,10 @@ function initShaderProgram(gl, vsSource, fsSource) {
   // If creating the shader program failed, alert
 
   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    alert(
+      "Unable to initialize the shader program: " +
+        gl.getProgramInfoLog(shaderProgram)
+    );
     return null;
   }
 
@@ -500,7 +538,9 @@ function loadShader(gl, type, source) {
   // See if it compiled successfully
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    alert(
+      "An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader)
+    );
     gl.deleteShader(shader);
     return null;
   }
