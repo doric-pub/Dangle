39aa67d0c3be1d3b6a4546750715ff5720fbb61d
王劲鹏 <jingpeng.wang@outlook.com>
2022-01-07 14:46:29 +0800

    remote to assets

--- example/src/three.js/webgl_effects_parallaxbarrier.ts
+++ example/src/three.js/webgl_effects_parallaxbarrier.ts
@@ -8,33 +8,29 @@ import {
   GestureContainer,
   gestureContainer,
   Color,
-  RemoteResource,
   imageDecoder,
+  AssetsResource,
 } from "doric";
 import { dangleView, DangleWebGLRenderingContext, vsync } from "dangle";
 
-import * as THREE from "three"
-import { ParallaxBarrierEffect } from "./jsm/effects/ParallaxBarrierEffect"
+import * as THREE from "three";
+import { ParallaxBarrierEffect } from "./jsm/effects/ParallaxBarrierEffect";
 
 @Entry
 class webgl_effects_parallaxbarrier extends Panel {
-
-  private gestureView?: GestureContainer
+  private gestureView?: GestureContainer;
 
   onShow() {
     navbar(context).setTitle("webgl_effects_parallaxbarrier");
   }
   build(rootView: Group) {
     vlayout([
-      this.gestureView = gestureContainer(
-        [],
-        {
-          layoutConfig: layoutConfig().just(),
-          width: 300,
-          height: 300,
-          backgroundColor: Color.BLACK
-        }
-      ),
+      (this.gestureView = gestureContainer([], {
+        layoutConfig: layoutConfig().just(),
+        width: 300,
+        height: 300,
+        backgroundColor: Color.BLACK,
+      })),
     ])
       .apply({
         layoutConfig: layoutConfig().fit().configAlignment(Gravity.Center),
@@ -43,32 +39,32 @@ class webgl_effects_parallaxbarrier extends Panel {
       })
       .in(rootView);
 
-    let self = this
+    let self = this;
     this.gestureView?.addChild(
       dangleView({
         onReady: async (gl: DangleWebGLRenderingContext) => {
-          const width = gl.drawingBufferWidth
-          const height = gl.drawingBufferHeight
+          const width = gl.drawingBufferWidth;
+          const height = gl.drawingBufferHeight;
 
-          const inputCanvas = 
-          ({
+          const inputCanvas = {
             width: width,
             height: height,
             style: {},
             addEventListener: (() => {}) as any,
             removeEventListener: (() => {}) as any,
             clientHeight: height,
-            getContext: (() => {return gl}) as any,
-          } as HTMLCanvasElement);
+            getContext: (() => {
+              return gl;
+            }) as any,
+          } as HTMLCanvasElement;
 
           let window = {
             innerWidth: width,
             innerHeight: height,
             devicePixelRatio: 1,
-            addEventListener: (() => {}) as any
-          }
-          let requestAnimationFrame = vsync(context).requestAnimationFrame
-
+            addEventListener: (() => {}) as any,
+          };
+          let requestAnimationFrame = vsync(context).requestAnimationFrame;
 
           //#region code to impl
           let container, camera, scene, renderer, effect;
@@ -93,125 +89,134 @@ class webgl_effects_parallaxbarrier extends Panel {
           animate();
 
           async function init() {
-
             // container = document.createElement( 'div' );
             // document.body.appendChild( container );
 
-            camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 100 );
+            camera = new THREE.PerspectiveCamera(
+              60,
+              window.innerWidth / window.innerHeight,
+              0.01,
+              100
+            );
             camera.position.z = 3;
             camera.focalLength = 3;
 
-            
-            const path = "https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/cube/pisa/";
-            const format = '.png';
+            const path = "threejs/pisa/";
+            const format = ".png";
             const urls = [
-              path + 'px' + format, path + 'nx' + format,
-              path + 'py' + format, path + 'ny' + format,
-              path + 'pz' + format, path + 'nz' + format
+              path + "px" + format,
+              path + "nx" + format,
+              path + "py" + format,
+              path + "ny" + format,
+              path + "pz" + format,
+              path + "nz" + format,
             ];
 
             var textures: any[] = [];
             for (let index = 0; index < urls.length; index++) {
               const url = urls[index];
-              const remoteResource = new RemoteResource(url)
-              const imageInfo = await imageDecoder(context).getImageInfo(remoteResource)
-              const imagePixels = await imageDecoder(context).decodeToPixels(remoteResource)
+              const assetsResource = new AssetsResource(url);
+              const imageInfo = await imageDecoder(context).getImageInfo(
+                assetsResource
+              );
+              const imagePixels = await imageDecoder(context).decodeToPixels(
+                assetsResource
+              );
 
-              const texture = new THREE.DataTexture(imagePixels, imageInfo.width, imageInfo.height, THREE.RGBAFormat);
-              textures.push(texture)
+              const texture = new THREE.DataTexture(
+                imagePixels,
+                imageInfo.width,
+                imageInfo.height,
+                THREE.RGBAFormat
+              );
+              textures.push(texture);
             }
-            
-            var textureCube = new THREE.CubeTexture( textures );
-		        textureCube.needsUpdate = true;
+
+            var textureCube = new THREE.CubeTexture(textures);
+            textureCube.needsUpdate = true;
 
             scene = new THREE.Scene();
             scene.background = textureCube;
 
-            const geometry = new THREE.SphereGeometry( 0.1, 32, 16 );
-            const material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube } );
+            const geometry = new THREE.SphereGeometry(0.1, 32, 16);
+            const material = new THREE.MeshBasicMaterial({
+              color: 0xffffff,
+              envMap: textureCube,
+            });
 
-            for ( let i = 0; i < 500; i ++ ) {
-
-              const mesh = new THREE.Mesh( geometry, material );
+            for (let i = 0; i < 500; i++) {
+              const mesh = new THREE.Mesh(geometry, material);
 
               mesh.position.x = Math.random() * 10 - 5;
               mesh.position.y = Math.random() * 10 - 5;
               mesh.position.z = Math.random() * 10 - 5;
 
-              mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
+              mesh.scale.x =
+                mesh.scale.y =
+                mesh.scale.z =
+                  Math.random() * 3 + 1;
 
-              scene.add( mesh );
+              scene.add(mesh);
 
-              spheres.push( mesh );
-
+              spheres.push(mesh);
             }
 
             //
 
-            renderer = new THREE.WebGLRenderer({canvas: inputCanvas});
-            renderer.setPixelRatio( window.devicePixelRatio );
+            renderer = new THREE.WebGLRenderer({ canvas: inputCanvas });
+            renderer.setPixelRatio(window.devicePixelRatio);
             // container.appendChild( renderer.domElement );
 
             const width = window.innerWidth || 2;
             const height = window.innerHeight || 2;
 
-            effect = new ParallaxBarrierEffect( renderer );
-            effect.setSize( width, height );
+            effect = new ParallaxBarrierEffect(renderer);
+            effect.setSize(width, height);
 
             //
 
-            window.addEventListener( 'resize', onWindowResize );
-
+            window.addEventListener("resize", onWindowResize);
           }
 
           function onWindowResize() {
-
             windowHalfX = window.innerWidth / 2;
             windowHalfY = window.innerHeight / 2;
 
             camera.aspect = window.innerWidth / window.innerHeight;
             camera.updateProjectionMatrix();
 
-            effect.setSize( window.innerWidth, window.innerHeight );
-
+            effect.setSize(window.innerWidth, window.innerHeight);
           }
 
-          function onDocumentMouseMove( event ) {
-
-            mouseX = ( event.clientX - windowHalfX ) / 100;
-            mouseY = ( event.clientY - windowHalfY ) / 100;
-
+          function onDocumentMouseMove(event) {
+            mouseX = (event.clientX - windowHalfX) / 100;
+            mouseY = (event.clientY - windowHalfY) / 100;
           }
 
           //
 
           function animate() {
-
-            requestAnimationFrame( animate );
+            requestAnimationFrame(animate);
 
             render();
-
           }
 
           function render() {
-
             const timer = 0.0001 * Date.now();
 
-            camera.position.x += ( mouseX - camera.position.x ) * .05;
-            camera.position.y += ( - mouseY - camera.position.y ) * .05;
+            camera.position.x += (mouseX - camera.position.x) * 0.05;
+            camera.position.y += (-mouseY - camera.position.y) * 0.05;
 
-            camera.lookAt( scene.position );
+            camera.lookAt(scene.position);
 
-            for ( let i = 0, il = spheres.length; i < il; i ++ ) {
+            for (let i = 0, il = spheres.length; i < il; i++) {
+              const sphere = spheres[i];
 
-              const sphere = spheres[ i ];
-
-              sphere.position.x = 5 * Math.cos( timer + i );
-              sphere.position.y = 5 * Math.sin( timer + i * 1.1 );
-
+              sphere.position.x = 5 * Math.cos(timer + i);
+              sphere.position.y = 5 * Math.sin(timer + i * 1.1);
             }
 
-            effect.render( scene, camera );
+            effect.render(scene, camera);
 
             gl.endFrame();
           }
@@ -223,7 +228,6 @@ class webgl_effects_parallaxbarrier extends Panel {
         width: 300,
         height: 300,
       })
-    )
+    );
   }
 }
-  
