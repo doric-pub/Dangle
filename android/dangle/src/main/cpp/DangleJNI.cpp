92172853a4f76a266aa853847409428c391ae99e
王劲鹏 <jingpeng.wang@outlook.com>
2022-01-27 11:26:45 +0800

    -fPIC & register natives

--- android/dangle/src/main/cpp/DangleJNI.cpp
+++ android/dangle/src/main/cpp/DangleJNI.cpp
@@ -7,31 +7,20 @@
 #include <jsi/jsi.h>
 #include "UDangle.h"
 
-extern "C" {
-
-JNIEXPORT jint JNICALL
-Java_pub_doric_dangle_DangleJNI_ContextCreate([[maybe_unused]] JNIEnv *env,
-                                              [[maybe_unused]] jclass clazz, jlong jsiPtr) {
+jint ContextCreate([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jclass clazz, jlong jsiPtr) {
     return UDangleContextCreate((void *) jsiPtr);
 }
 
-JNIEXPORT void JNICALL
-Java_pub_doric_dangle_DangleJNI_ContextDestroy([[maybe_unused]] JNIEnv *env,
-                                               [[maybe_unused]] jclass clazz, jint dangleCtxId) {
+void ContextDestroy([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jclass clazz, jint dangleCtxId) {
     UDangleContextDestroy(dangleCtxId);
 }
 
-JNIEXPORT void JNICALL
-Java_pub_doric_dangle_DangleJNI_ContextFlush([[maybe_unused]] JNIEnv *env,
-                                             [[maybe_unused]] jclass clazz, jint dangleCtxId) {
+void ContextFlush([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jclass clazz, jint dangleCtxId) {
     UDangleContextFlush(dangleCtxId);
 }
 
-JNIEXPORT void JNICALL Java_pub_doric_dangle_DangleJNI_ContextSetFlushMethod(
-        JNIEnv *env,
-        [[maybe_unused]] jclass clazz,
-        jint dangleCtxId,
-        jobject glContext) {
+void ContextSetFlushMethod(JNIEnv *env, [[maybe_unused]] jclass clazz, jint dangleCtxId,
+                           jobject glContext) {
     jclass GLContextClass = env->GetObjectClass(glContext);
     jobject glContextRef = env->NewGlobalRef(glContext);
     jmethodID flushMethodRef = env->GetMethodID(GLContextClass, "flush", "()V");
@@ -42,16 +31,52 @@ JNIEXPORT void JNICALL Java_pub_doric_dangle_DangleJNI_ContextSetFlushMethod(
     UDangleContextSetFlushMethod(dangleCtxId, flushMethod);
 }
 
-JNIEXPORT jboolean JNICALL
-Java_pub_doric_dangle_DangleJNI_ContextNeedsRedraw([[maybe_unused]] JNIEnv *env,
-                                                   [[maybe_unused]] jclass clazz,
-                                                   jint dangleCtxId) {
+jboolean
+ContextNeedsRedraw([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jclass clazz, jint dangleCtxId) {
     return UDangleContextNeedsRedraw(dangleCtxId);
 }
 
-JNIEXPORT void JNICALL
-Java_pub_doric_dangle_DangleJNI_ContextDrawEnded([[maybe_unused]] JNIEnv *env,
-                                                 [[maybe_unused]] jclass clazz, jint dangleCtxId) {
+void
+ContextDrawEnded([[maybe_unused]] JNIEnv *env, [[maybe_unused]] jclass clazz, jint dangleCtxId) {
     UDangleContextDrawEnded(dangleCtxId);
 }
+
+static JNINativeMethod methods[] = {
+        {"ContextCreate",         "(J)I",                   (void *) &ContextCreate},
+        {"ContextDestroy",        "(I)V",                   (void *) &ContextDestroy},
+        {"ContextFlush",          "(I)V",                   (void *) &ContextFlush},
+        {"ContextSetFlushMethod", "(ILjava/lang/Object;)V", (void *) &ContextSetFlushMethod},
+        {"ContextNeedsRedraw",    "(I)Z",                   (void *) &ContextNeedsRedraw},
+        {"ContextDrawEnded",      "(I)V",                   (void *) &ContextDrawEnded},
+};
+
+int jniRegisterNativeMethods(JNIEnv *env, const char *className, const JNINativeMethod *gMethods,
+                             int numMethods) {
+    jclass clazz;
+    int tmp;
+    clazz = env->FindClass(className);
+    if (clazz == nullptr) {
+        return -1;
+    }
+    if ((tmp = env->RegisterNatives(clazz, gMethods, numMethods)) < 0) {
+        return -1;
+    }
+    return 0;
+}
+
+int registerNativeMethods(JNIEnv *env) {
+    return jniRegisterNativeMethods(env, "pub/doric/dangle/DangleJNI",
+                                    methods,
+                                    sizeof(methods) / sizeof(methods[0]));
+}
+
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *env;
+    if (vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK) {
+        return -1;
+    }
+    if (registerNativeMethods(env) != JNI_OK) {
+        return -1;
+    }
+    return JNI_VERSION_1_6;
 }
